11b08b82b653060fda3c97a023cc60e5d7951011
Veronica Inzunza <vinzunza666@gmail.com>
2024-03-27 22:03:39 -0300

    Segundo commit tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 // Smooth scroll
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -12,3 +19,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       }
     });
   });
+
+// tooltips
+
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
